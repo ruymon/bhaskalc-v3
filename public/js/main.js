d00ad42d7dc11b868786d9456ef566bf22495161
ruymon <ruystfrancis@gmail.com>
2020-11-28 22:40:08 -0300

    V3 - Major Fixes

--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,23 @@ function loadingScreenTimer() {
     setTimeout(() => {
         document.getElementById('page-loader').style.display = 'none';
         document.getElementById('page-input').style.display = 'flex';
-    }, 2900)
-};
+        }, 2900)
+    };
 
 // Calculate Screen Size
 
-var browserWidth;
-var browserHeight;
+let browserWidth;
+let browserHeight;
 
-var outputSidebarWidth;
-var outputSidebarHeight;
+let outputSidebarWidth;
+let outputSidebarHeight;
 
-var outputButtonsWidth;
-var outputButtonsHeight;
+let outputButtonsWidth;
+let outputButtonsHeight;
 
 
 function calculateOutputSizes() {
+
     //Get Browser Windos Sizes
     browserWidth = $(window).width();
     browserHeight = $(window).height();
@@ -28,7 +29,7 @@ function calculateOutputSizes() {
     var outputSidebar = document.getElementById('output-aside');
     var sidebarPositionInfo = outputSidebar.getBoundingClientRect();
     outputSidebarWidth = sidebarPositionInfo.width;
-    outputSidebarWidth = sidebarPositionInfo.height;
+    outputSidebarHeight = sidebarPositionInfo.height;
 
     //Get Button Div size
     var outputButtons = document.getElementById('output-buttons-container');
@@ -36,50 +37,4 @@ function calculateOutputSizes() {
     outputButtonsWidth = buttonsPositionInfo.width;
     outputButtonsHeight = buttonsPositionInfo.height;
 
-
-    
-    // console.log("Browser Width = " + browserWidth);
-    // console.log("Browser Height = " + browserHeight);
-
-    // console.log("Sidebar Width = " + outputSidebarWidth);
-    // console.log("Sidebar Height = " + outputSidebarHeight);
-
-    // console.log("Buttons Width = " + outputButtonsWidth);
-    // console.log("Buttons Height = " + outputButtonsHeight);
-
-
-    // console.log("------------------------------");
-
-
 };
-
-
-
-
-// function graph
-
-
-// var element = document.getElementById('output-aside');
-// var positionInfo = element.getBoundingClientRect();
-// var height = positionInfo.height;
-// var width = positionInfo.width;
-
-// console.log(height, width);
-
-
-// REFERENCES //
-
-// To get Whole Screen Size
-
-    // window.screen.height;
-    // window.screen.width;
-
-// To get Browser Viewport [jQuery]
-
-    // $(window).height();
-    // $(window).width();
-
-// To get HTML sizes
-
-    // $(document).height();
-    // $(document).width();
--- public/js/second-degree.js
+++ public/js/second-degree.js
@@ -3,6 +3,13 @@
 // const B = document.getElementById("sd-b").value;
 // const C = document.getElementById("sd-c").value;
 
+// Input Var (Global)
+
+var A;
+var B;
+var C;
+
+
 //the results:
 //b2 - 4ac. DELTA CAN NOT BE NEGATIVE YET!
 var delta;
@@ -39,48 +46,49 @@ function calculateVertex(A, B, delta) {
 }
 
 function calculate() {
+
     document.getElementById('page-input').style.display = 'none';
     document.getElementById('page-output').style.display = 'flex';
 
     calculateOutputSizes()
 
-    setTimeout(() =>{
-        const A = document.getElementById("sd-a").value;
-        const B = document.getElementById("sd-b").value;
-        const C = document.getElementById("sd-c").value;
-    
-        // Input Validation
-        if (isNaN(A) || isNaN(B) || isNaN(C)) {
-            alert('Faltando parametros.');
-            returnToInputPage();
-            return;
-        }
-    
-        if (A == 0) {
-            alert('O Coeficiente A não pode ser 0.');
-            returnToInputPage();
-            return;
-        };
-    
-    
-        calculateDelta(A, B, C);
-        calculateRoots(A, B, delta);
-        calculateVertex(A, B, delta);
-        //the y intercept is calculated by x = 0, so intercept = C.
-        intercept = C;
-    
-        
-    
-        console.log("Delta: " + delta);
-        console.log("Roots: " + root1 + " , " + root2);
-        console.log("Vertexes: " + vertexX.toString() + " , " + vertexY.toString());
-        console.log("Intercept: " + C);
-    
 
-    
-        showOutput(delta, root1, root2, vertexX, vertexY, A, B, C);
+    A = document.getElementById("sd-a").value;
+    B = document.getElementById("sd-b").value;
+    C = document.getElementById("sd-c").value;
 
-    }, 100);
+
+    // Input Validation
+    if (isNaN(A) || isNaN(B) || isNaN(C)) {
+        alert('Faltando parametros.');
+        returnToInputPage();
+        return;
+    }
+
+    if (A == 0) {
+        alert('O Coeficiente A não pode ser 0.');
+        returnToInputPage();
+        return;
+    };
+
+
+    calculateDelta(A, B, C);
+    calculateRoots(A, B, delta);
+    calculateVertex(A, B, delta);
+    //the y intercept is calculated by x = 0, so intercept = C.
+    intercept = C;
+
+
+
+    console.log("Delta: " + delta);
+    console.log("Roots: " + root1 + " , " + root2);
+    console.log("Vertexes: " + vertexX.toString() + " , " + vertexY.toString());
+    console.log("Intercept: " + C);
+
+
+
+    showOutput(browserHeight, browserWidth, outputButtonsWidth, outputSidebarWidth, delta, root1, root2, vertexX, vertexY, A, B, C);
+
 }
 
 
@@ -88,27 +96,36 @@ function calculate() {
 
 // Show Calculated Data
 
-function showOutput(graphWidth, graphHeight, delta, root1, root2, vertexX, vertexY, A, B, C) {
+function showOutput(browserHeight, browserWidth, outputButtonsWidth, outputSidebarWidth, delta, root1, root2, vertexX, vertexY, A, B, C) {
+
     document.getElementById('output-roots').innerHTML = '(' + root1 + ',' + root2 + ')';
     document.getElementById('output-vertex').innerHTML = '(' + vertexX.toString() + ',' + vertexY.toString() + ')';
     document.getElementById('output-delta').innerHTML = delta;
 
+    const graphWidth = browserWidth - (outputSidebarWidth + outputButtonsWidth);
+    const graphHeight = browserHeight - 10;
+
     // Make Graph
     functionPlot({
-        target: document.querySelector("#graph"),
-        // width: `${(outputSidebarWidth - outputButtonsWidth) - 20}`,
-        // height: `${browserHeight - 10}`,
-        width: 500,
-        height: 500,
+        target: "#graph",
+        width: graphWidth,
+        height: graphHeight,
         yAxis: {
             domain: [-1, 9]
         },
         grid: true,
         data: [{
             // fn: 'x^2 + 2x + 3', // f(x) = ax² + bx + c
-            fn: `${A}x^2 + ${B}x + ${C}`
+            fn: `${A}x^2 + ${B}x + ${C}`,
         }]
     });
+
+    const svg = document.querySelector("#graph svg");
+    if (svg) {
+        svg.classList.add("graphStyle");
+    }
+
+
 
 }
 
